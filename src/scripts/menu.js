3954249af7d5ed9d2d8db5011b6ab1d2e12a2969
connorwarme <connor.warme@gmail.com>
2023-02-28 19:23:17 -0700

    Working on mobile menu display

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,6 @@
 document.querySelector('div.mobile-hamburger').addEventListener('click', () => {
-  document.querySelector('div.nav-links').classList.toggle('expanded');
+  document.querySelector('nav').classList.add('expanded');
+})
+document.querySelector('div.nav-overlay').addEventListener('click', () => {
+  document.querySelector('nav').classList.remove('expanded');
 })
