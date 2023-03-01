cd6b6e8028950f5c07bda5fe21831fccb65a17c6
connorwarme <connor.warme@gmail.com>
2023-03-01 11:06:49 -0700

    Adjust header margin; hide arrow on overlay

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,6 +1,8 @@
 document.querySelector('div.mobile-hamburger').addEventListener('click', () => {
   document.querySelector('nav').classList.add('expanded');
+  document.querySelector('div.scroll-container').style.visibility = "hidden";
 })
 document.querySelector('div.nav-overlay').addEventListener('click', () => {
   document.querySelector('nav').classList.remove('expanded');
+  document.querySelector('div.scroll-container').style.visibility = "visible";
 })
