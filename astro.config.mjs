f95446d1f7cd82ce304dc5e20d178b1828c29db0
connorwarme <connor.warme@gmail.com>
2023-03-22 15:46:46 -0600

    Add mdx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
+  integrations: [preact(), mdx()],
   site: 'https://connorwarme.github.io',
-  base: '/pro-site',
+  base: '/pro-site'
 });
